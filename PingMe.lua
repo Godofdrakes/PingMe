15448f39fccab3eeae1194cff14544482d8c7f6a
Urias <seven.bit.gaming@gmail.com>
2013-08-10 11:46:27 -0800

    v6.1
    Added keybind (somewhat working)

--- PingMe.lua
+++ PingMe.lua
@@ -14,6 +14,7 @@ require "lib/lib_InterfaceOptions";
 require "lib/lib_MapMarker";
 require "lib/lib_Callback2";
 require "lib/lib_NavWheel";
+require "lib/lib_UserKeybinds"
 
 --Variables
 local PING = {};
@@ -25,6 +26,7 @@ local openSquad = true
 local scanPublic = true
 local openPublic = false
 local dev = true --Scan system chat.
+local loaded = false
 
 --Map Markers
 local army_marker = {}
@@ -33,11 +35,45 @@ local public_marker = {}
 local system_marker = {}
 local misc_marker = {}
 
+--Key Bind
+local FUNCTION_KEYS = {
+	["F1"] = {NUM=112, TAB="Function Keys"},
+	["F2"] = {NUM=113, TAB="Function Keys"},
+	["F3"] = {NUM=114, TAB="Function Keys"},
+	["F4"] = {NUM=115, TAB="Function Keys"},
+	["F5"] = {NUM=116, TAB="Function Keys"},
+	["F6"] = {NUM=117, TAB="Function Keys"},
+	["F7"] = {NUM=118, TAB="Function Keys"},
+	["F8"] = {NUM=119, TAB="Function Keys"},
+	["F9"] = {NUM=120, TAB="Function Keys"},
+	["F10"] = {NUM=121, TAB="Function Keys"},
+	["F11"] = {NUM=122, TAB="Function Keys"},
+	["F12"] = {NUM=123, TAB="Function Keys"},
+}
+
+local KEY_ID = FUNCTION_KEYS["F5"]["NUM"]
+local KEY_BIND = {}
+
 --Nodes
 local ping_nodes = {}
 
 --Core Functions
 InterfaceOptions.AddCheckBox({id="ENABLESCAN", label="Enable scanning of pings", default=true, tooltip="Disables all scanning of recieved pings. You can still ping your loaction to others."})
+InterfaceOptions.AddChoiceMenu({id="FUNCTION_KEY", label="Bind to", tooltip="Pressing this key will automatically open the PingMe node in the NavWheel. Usefull if it disappears for some reason.",})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F1", val=112})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F2", val=113})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F3", val=114})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F4", val=115})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F5", val=116})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F6", val=117})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F7", val=118})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F8", val=119})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F9", val=120})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F10", val=121})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F11", val=122})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="F12", val=123})
+InterfaceOptions.AddChoiceEntry({menuId="FUNCTION_KEY", label="NONE", val=nil})
+InterfaceOptions.NotifyOnLoaded(true)
 
 function SetOptionsAvailability()
 end
@@ -55,23 +91,32 @@ function OnComponentLoad()
 	LIB_SLASH.BindCallback({slash_list="clearpublic", description="Removes the currently saved public ping.", func=PING.ClearPublic})
 	LIB_SLASH.BindCallback({slash_list="clearpings", description="Removes all currently saved pings.", func=PING.ClearPings})
 	SetOptionsAvailability()
-	InitNavNodes()
 end
 
 function OnMessage(args)
-	if (args.type == "ENABLESCAN") then
-		enableScanning = (args.data == true or args.data == "true")
+	local option = args.type
+	local message = args.data
+	if (option == "__LOADED") then
+		InitNavNodes()
+		BindKey()
+		loaded = true
+		return nil
+	end
 
-	elseif (args.type == "SCAN_ARMY") then scanArmy = args.data
-	elseif (args.type == "OPEN_ARMY") then openArmy = args.data
+	if (option == "ENABLESCAN") then enableScanning = (message == true or message == "true")
 
-	elseif (args.type == "SCAN_SQUAD") then scanSquad = args.data
-	elseif (args.type == "OPEN_SQUAD") then openSquad = args.data
+	elseif (option == "SCAN_ARMY") then scanArmy = message
+	elseif (option == "OPEN_ARMY") then openArmy = message
 
-	elseif (args.type == "SCAN_PUBLIC") then scanPublic = args.data
-	elseif (args.type == "OPEN_PUBLIC") then openPublic = args.data
+	elseif (option == "SCAN_SQUAD") then scanSquad = message
+	elseif (option == "OPEN_SQUAD") then openSquad = message
 
-	elseif (args.type == "DEV") then dev = args.data
+	elseif (option == "SCAN_PUBLIC") then scanPublic = message
+	elseif (option == "OPEN_PUBLIC") then openPublic = message
+
+	elseif (option == "DEV") then dev = message
+
+	elseif (option == "FUNCTION_KEY") then KEY_ID = message; BindKey()
 
 	end
 	SetOptionsAvailability()
@@ -150,11 +195,10 @@ function puts(buffer, chatOutput)
 	--Makes chat messages. Credit to LemonKing
 	--if chatOutput is undefined broadcast will be used
 	if type(chatOutput) ~= "string" then chatOutput = nil end
-	Component.GenerateEvent("MY_CHAT_MESSAGE", {channel=chatOutput or "system", text=buffer})
+	Component.GenerateEvent("MY_CHAT_MESSAGE", {channel=chatOutput or "system", text=tostring(buffer)})
 end
 
 function MakeMarker(args) --args = {id, channel, title, subtitle, x, y, z}
-	warn(tostring(args))
 	if (args.channel == "army" and army_marker[args.id].map == nil) then
 		army_marker[args.id].map = MapMarker.Create()
 		army_marker[args.id].map:BindToPosition({x=args.x, y=args.y, z=args.z})
@@ -323,41 +367,62 @@ function MarkerNode(args) --args = {id, channel, title, subtitle, x, y, z}
 end
 
 function InitNavNodes()
-	ping_nodes.base = NavWheel.CreateNode("pingme_base")
+	ping_nodes.base = NavWheel.CreateNode()
 	ping_nodes.base:SetTitle("PingMe")
 	ping_nodes.base:SetDescription("Recieved Markers")
 	ping_nodes.base:SetParent("hud_root")
 	ping_nodes.base:GetIcon():SetTexture("icons", "ping")
 
-	ping_nodes.army = NavWheel.CreateNode("army_markers")
+	ping_nodes.army = NavWheel.CreateNode()
 	ping_nodes.army:SetTitle("Army Pings")
 	ping_nodes.army:SetDescription("Pings recieved through army chat.")
-	ping_nodes.army:SetParent("pingme_base")
+	ping_nodes.army:SetParent(ping_nodes.base:GetId())
 	ping_nodes.army:GetIcon():SetTexture("icons", "Army")
 
-	ping_nodes.squad = NavWheel.CreateNode("squad_markers")
+	ping_nodes.squad = NavWheel.CreateNode()
 	ping_nodes.squad:SetTitle("Squad Pings")
 	ping_nodes.squad:SetDescription("Pings recieved through squad chat.")
-	ping_nodes.squad:SetParent("pingme_base")
+	ping_nodes.squad:SetParent(ping_nodes.base:GetId())
 	ping_nodes.squad:GetIcon():SetTexture("aag_icons", "squad")
 
-	ping_nodes.public = NavWheel.CreateNode("public_markers")
+	ping_nodes.public = NavWheel.CreateNode()
 	ping_nodes.public:SetTitle("Public Pings")
 	ping_nodes.public:SetDescription("Pings recieved through public chat channels.")
-	ping_nodes.public:SetParent("pingme_base")
+	ping_nodes.public:SetParent(ping_nodes.base:GetId())
 	ping_nodes.public:GetIcon():SetTexture("icons", "com_tower")
 
 	if (dev) then
-		ping_nodes.system = NavWheel.CreateNode("system_markers")
+		ping_nodes.system = NavWheel.CreateNode()
 		ping_nodes.system:SetTitle("System Pings")
 		ping_nodes.system:SetDescription("Pings recieved through system chat.")
-		ping_nodes.system:SetParent("pingme_base")
+		ping_nodes.system:SetParent(ping_nodes.base:GetId())
 		ping_nodes.system:GetIcon():SetTexture("icons", "refining")
 	end
 
-	ping_nodes.misc = NavWheel.CreateNode("misc_markers")
+	ping_nodes.misc = NavWheel.CreateNode()
 	ping_nodes.misc:SetTitle("Misc Pings")
 	ping_nodes.misc:SetDescription("Pings recieved through other chat channels.")
-	ping_nodes.misc:SetParent("pingme_base")
+	ping_nodes.misc:SetParent(ping_nodes.base:GetId())
 	ping_nodes.misc:GetIcon():SetTexture("icons", "game")
+end
+
+function BindKey()
+	KEY_BIND.FUNC = function()
+		NavWheel.OpenNode(ping_nodes.base:GetId())
+	end
+
+	if (loaded) then
+		KEY_BIND.BIND:Destroy()
+		KEY_BIND.BIND = UserKeybinds.Create()
+		KEY_BIND.BIND:RegisterAction("OPEN_PINGME", KEY_BIND.FUNC)
+		if (KEY_ID ~= "NONE" and KEY_ID ~= nil) then
+			KEY_BIND.BIND:BindKey("OPEN_PINGME", tonumber(KEY_ID))
+		end
+	else
+		KEY_BIND.BIND = UserKeybinds.Create()
+		KEY_BIND.BIND:RegisterAction("OPEN_PINGME", KEY_BIND.FUNC)
+		if (KEY_ID ~= "NONE" and KEY_ID ~= nil) then
+			KEY_BIND.BIND:BindKey("OPEN_PINGME", tonumber(KEY_ID))
+		end
+	end
 end
